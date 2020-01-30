b8ecfe72a033ab6e75752ac95a07d6f9487ec7f0
nsbernstein52 <nsbernstein52@gmail.com>
2020-01-30 14:51:28 -0700

    Code flatten (array), bugs: bad output (bug in test?), calls only once

--- src/recursion.js
+++ src/recursion.js
@@ -833,7 +833,17 @@ console.log(nestedEvenSum(obj1)); // 10
 // 30. Flatten an array containing nested arrays.
 // flatten([1,[2],[3,[[4]]],5]); // [1,2,3,4,5]
 var flatten = function(array) {
+  return arr.reduce((flatArr, i) => {
+    if (Array.isArray(i)) {
+      return flatArr.concat(flatten(i));
+    }
+  return flatArr.concat(i)
+  }, []);
 };
+// flatten recursively: data & tests
+console.log(flatten([1,[2],[3,[[4]]],5])); // [1,2,3,4,5]
+console.log(flatten([[1],[2,3],[[4]],5])); // [1,2,3,4,5]
+console.log(flatten([[1],[2,[],3],[],[[4]],5])); // [1,2,3,4,5]
 
 // 31. Given a string, return an object containing tallies of each letter.
 // letterTally('potato'); // {p:1, o:2, t:2, a:1}
